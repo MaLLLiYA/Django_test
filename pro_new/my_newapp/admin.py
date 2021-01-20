b0ccb6cdde394928bc4f7ec68ae03f48cb291818
mali <1542307217@qq.com>
2021-01-21 01:04:49 +0800

    new project

---
+++ pro_new/my_newapp/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+
+# Register your models here.
+from my_newapp.models import Student
+
+admin.site.register(Student)
---
+++ pro_new/my_newapp/urls.py
@@ -0,0 +1,6 @@
+from my_newapp.views import index
+from django.urls import path
+
+urlpatterns = [
+    path('index/', index)
+]
