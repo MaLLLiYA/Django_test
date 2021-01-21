0d8a0ac59eabfb9f3b80e8a93aefa3e13f853cfe
mali <1542307217@qq.com>
2021-01-21 16:38:13 +0800

    create table bookinfo

--- pro_new/my_newapp/models.py
+++ pro_new/my_newapp/models.py
@@ -9,3 +9,21 @@ class Student(models.Model):
 
     def __str__(self):
         return self.name
+
+    class Meta:
+        db_table = 'student'
+
+
+class Bookinfo(models.Model):
+    name = models.CharField(max_length=20, unique=True)
+    pub_date = models.DateField(null=True)
+    read_count = models.IntegerField(default=0)
+    comment_count = models.IntegerField(default=0)
+    is_deleted = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table = 'bookinfo'
+        verbose_name = '书籍管理'
