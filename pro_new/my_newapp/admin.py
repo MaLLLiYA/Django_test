a355d24995480e26cb4164de85513a36505e692f
mali <1542307217@qq.com>
2021-01-21 17:19:15 +0800

    some changes

--- pro_new/my_newapp/admin.py
+++ pro_new/my_newapp/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from my_newapp.models import Student
+from my_newapp.models import Student, Bookinfo,Peopleinfo
 
 admin.site.register(Student)
+admin.site.register(Bookinfo)
+admin.site.register(Peopleinfo)
